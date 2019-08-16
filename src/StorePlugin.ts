f515bfcdde2df66c59e74ed23bc90b1b5df2cdd5
Alan Holt <holtalanm@gmail.com>
2019-08-16 00:56:44 -0500

    fixed types

--- src/StorePlugin.ts
+++ src/StorePlugin.ts
@@ -1,5 +1,9 @@
 import Store from './Store';
 
+/**
+ * Event fired by the Store on a value change.
+ * This could have been fired before or after the change.
+ */
 export interface StorePluginValueChangeEvent<V> {
     store: Store;
     property: string;
@@ -7,8 +11,28 @@ export interface StorePluginValueChangeEvent<V> {
     newValue: V;
 }
 
+/**
+ * A plugin for Comstock.  Can be used to provide additional functionality to a Store,
+ * such as storing the state into localStorage.
+ */
 export interface StorePlugin {
+    /**
+     * Method called when the store this plugin is registered to is first constructed.
+     * This is a good time for the plugin to set an initial state into the store or something.
+     *
+     * @param store the Store object that was initialized
+     */
     onStoreInitialized?(store: Store): Promise<void>;
+    /**
+     * Method called when a value is about to be changed within the store state.
+     *
+     * @param event the value change event
+     */
     beforeValueChange?(event: StorePluginValueChangeEvent<any>): Promise<void>;
+    /**
+     * Method called after a value is changed within the store state.
+     *
+     * @param event the value change event
+     */
     afterValueChange?(event: StorePluginValueChangeEvent<any>): Promise<void>;
 }
